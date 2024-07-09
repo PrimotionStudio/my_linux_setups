4973bac7fec9db6f31b46da6c7560bbc4ed9fed9
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-09 03:26:31 +0100

    "Added install_mongodb.sh script to install MongoDB 6.0 on Ubuntu"

---
+++ install_mongodb.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+# -*- coding: utf-8 -*-
+# @Author:              Prime
+# @Date:                2023-06-09 03:22
+# @Last Modified by:    Prime
+# @Last Modified time:  2023-06-09 03:22
+# @Email:               oyedelenewton@gmail.com
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo tee /etc/apt/trusted.gpg.d/mongodb-org-6.0.asc
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+mongosh
